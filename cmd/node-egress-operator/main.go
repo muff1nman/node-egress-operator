752790e6f2a7dd7e021a6c0be74c6aafe6c00dca
Andrew DeMaria <lostonamountain@gmail.com>
2018-08-07 17:57:18 -0400

    Adjusted formatting

--- cmd/node-egress-operator/main.go
+++ cmd/node-egress-operator/main.go
@@ -1,33 +1,34 @@
 package main
 
 import (
-	"context"
-	"runtime"
+  "context"
+  "runtime"
 
-	stub "github.com/muff1nman/node-egress-operator/pkg/stub"
-	sdk "github.com/operator-framework/operator-sdk/pkg/sdk"
-	sdkVersion "github.com/operator-framework/operator-sdk/version"
+  stub "github.com/muff1nman/node-egress-operator/pkg/stub"
+  sdk "github.com/operator-framework/operator-sdk/pkg/sdk"
+  sdkVersion "github.com/operator-framework/operator-sdk/version"
 
-	"github.com/sirupsen/logrus"
-	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
+  "github.com/sirupsen/logrus"
+  _ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 )
 
 func printVersion() {
-	logrus.Infof("Go Version: %s", runtime.Version())
-	logrus.Infof("Go OS/Arch: %s/%s", runtime.GOOS, runtime.GOARCH)
-	logrus.Infof("operator-sdk Version: %v", sdkVersion.Version)
+  logrus.Infof("Go Version: %s", runtime.Version())
+  logrus.Infof("Go OS/Arch: %s/%s", runtime.GOOS, runtime.GOARCH)
+  logrus.Infof("operator-sdk Version: %v", sdkVersion.Version)
 }
 
 func main() {
-	printVersion()
+  logrus.SetFormatter(&logrus.JSONFormatter{})
+  printVersion()
 
-	sdk.ExposeMetricsPort()
+  sdk.ExposeMetricsPort()
 
-	resource := "v1"
-	kind := "Node"
-	resyncPeriod := 5
-	logrus.Infof("Watching %s, %s, %s, %d", resource, kind, "", resyncPeriod)
-	sdk.Watch(resource, kind, "", resyncPeriod)
-	sdk.Handle(stub.NewHandler())
-	sdk.Run(context.TODO())
+  resource := "v1"
+  kind := "Node"
+  resyncPeriod := 5
+  logrus.Infof("Watching %s, %s, %s, %d", resource, kind, "", resyncPeriod)
+  sdk.Watch(resource, kind, "", resyncPeriod)
+  sdk.Handle(stub.NewHandler())
+  sdk.Run(context.TODO())
 }
